0474cce1991ebf5ac15194ea18cb50f34c4a03b5
Junior Pinheiro <juniorpinheiro7@outlook.com>
2025-05-23 11:17:03 -0300

    CRUD PRODUCTS

--- src/src/Controller/ProductController.php
+++ src/src/Controller/ProductController.php
@@ -2,19 +2,67 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/api/products')]
 final class ProductController extends AbstractController
 {
     #[Route('', methods: ['GET'])]
-    public function index(): JsonResponse
+    public function index(ProductRepository $repo): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller! updated',
-            'path' => 'src/Controller/ProductController.php',
-        ]);
+        $products = $repo->findAll();
+        return $this->json($products, 200);
+    }
+
+    #[Route('', methods:['POST'])]
+    public function create(Request $request, EntityManagerInterface $em)
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $product = new Product();
+        $product->setName($data['name']);
+        $product->setPrice($data['price']);
+        $product->setDescription($data['description'] ?? null);
+        $product->setCreatedAt(new \DateTimeImmutable());
+
+        $em->persist($product);
+        $em->flush();
+
+        return $this->json($product, 201);
+    }
+
+    #[Route('/{id}', methods: ['GET'])]
+    public function show(Product $product): JsonResponse
+    {
+        return $this->json($product, 200);
+    }
+
+    #[Route('/{id}', methods: ['PUT'])]
+    public function update(Request $request, Product $product, EntityManagerInterface $em)
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $product->setName($data['name'] ?? $product->getName());
+        $product->setPrice($data['price'] ?? $product->getPrice());
+        $product->setDescription($data['description'] ?? $product->getDescription());
+
+        $em->flush();
+
+        return $this->json($product, 200);
+    }
+
+    #[Route('/{id}', methods: ['DELETE'])]
+    public function delete(Product $product, EntityManagerInterface $em): JsonResponse
+    {
+        $em->remove($product);
+        $em->flush();
+
+        return $this->json(null, 204);
     }
 }
