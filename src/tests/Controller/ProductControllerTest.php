f1abe31e2a4d241166fb66111705d56f20809782
Junior Pinheiro <juniorpinheiro7@outlook.com>
2025-06-14 10:04:13 -0300

    preparando ambiente para testes e mock.

--- src/tests/Controller/ProductControllerTest.php
+++ src/tests/Controller/ProductControllerTest.php
@@ -6,14 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 final class ProductControllerTest extends WebTestCase
 {
-    public function testIndex(): void
-    {
-
+    public function testIndexReturnsPaginatedProducts(): void{
         $client = static::createClient();
-        $client->request('GET', '/api/products');
+
+        $client->request('GET', '/api/products?page=1');
 
         $this->assertResponseIsSuccessful();
-        $this->assertResponseFormatSame('json');
+        $this->assertResponseHeaderSame('Content-Type', 'application/json');
 
+        $data = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertIsArray($data);
+
+        $this->assertCount(10, $data);
+        $this->assertArrayHasKey('id', $data[0]);
     }
 }
